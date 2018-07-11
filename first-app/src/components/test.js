7e700dd33a3cff1efdc48d84819feb81a40a7985
dibyendu1993 <dibendu.kundu@gmail.com>
2018-07-11 11:38:01 +0530

    final commit

--- first-app/src/components/test.js
+++ first-app/src/components/test.js
@@ -1,40 +1,50 @@
 import React from 'react'
-import { Grid, Image, Icon, Button, Segment, Dimmer, Loader, Popup, Header } from 'semantic-ui-react'
+import { Component } from 'react'
+import { Grid, Modal, Icon, Button, Segment, Step, Loader, Popup, Header } from 'semantic-ui-react'
 import '../componentsCSS/test.css'
 
-const Practice = () => (
-  <div style={{maxWidth:"80vw",margin:"auto"}}>
-  <Grid divided>
+class ModalExampleControlled extends Component {
+  state = { modalOpen: false }
+
+  handleOpen = () => this.setState({ modalOpen: true })
+
+  handleClose = () => this.setState({ modalOpen: false })
+
+  render() {
     
-      <Grid.Column className='lefticon' computer="16">
+    return (
+      <div style={{maxWidth:"80vw",margin:"auto"}}>
+  <Grid  style={{marginTop: '1rem', border:'solid .1px #00000040'}}>
+    
+      <Grid.Column className='lefticon' style={{border:'solid .1px #00000040'}} computer="16">
       <Icon circular name='users'/>
-      <Icon circular className='righticon' name='sign in' style={{marginRight: "186px"}}/>
+      <Icon circular className='righticon' name='sign in' style={{marginRight: "45px"}}/>
       <Icon circular className='righticon' name='sign out'/>
       </Grid.Column>
-      <Grid.Column computer="4">
+      <Grid.Column computer="4" style={{border:'solid .1px #00000040'}}>
      
-     <div> <Button.Group>
+     <div style={{paddingLeft: '44px'}}> <Button.Group>
     <Button  icon='play' />
     <Button icon='pause' />
     <Button icon='shuffle' />
   </Button.Group>
   </div>
   <div>
-  <Button.Group>
+  <Button.Group style={{paddingLeft: '23px'}}>
     <Button negative>Cancel</Button>
     <Button.Or />
     <Button positive>Save</Button>
   </Button.Group>
   </div>
       </Grid.Column>
-      <Grid.Column computer="4">
+      <Grid.Column computer="4" style={{border:'solid .1px #00000040'}}>
       <div>
       <Segment style={{padding:"2em 1em"}}>
       <Loader active/>  
   </Segment>
   </div>
       </Grid.Column>
-      <Grid.Column computer="4">
+      <Grid.Column computer="4" style={{border:'solid .1px #00000040'}}>
      <div>
      <Icon circular name='google play'/>
      </div>
@@ -45,8 +55,9 @@ const Practice = () => (
      <Icon circular name='google play'/>
      </div>
       </Grid.Column>
-      <Grid.Column computer="4">
-      <Popup trigger={<Button>plans</Button>} flowing hoverable>
+      <Grid.Column computer="4" style={{border:'solid .1px #00000040'}}>
+      <div>
+      <Popup trigger={<Button style={{marginBottom: "12px"}}>choose your plan</Button>}  flowing hoverable>
     <Grid centered divided columns={3}>
       <Grid.Column textAlign='center'>
         <Header as='h4'>Basic Plan</Header>
@@ -71,17 +82,105 @@ const Practice = () => (
       </Grid.Column>
     </Grid>
   </Popup>
+  </div>
+  <div>
+  <Button onClick={this.handleOpen.bind(this)} >choose your plan</Button>
+     <Modal
+        open={this.state.modalOpen}
+       // onClose={this.handleClose}
+        basic
+        size='small'
+      >
+        <Header icon='browser' content='subscription you can choose' />
+        <Modal.Content>
+        <Grid centered divided columns={4}>
+
+      <Grid.Column textAlign='center'>
+       
+        <p>
+        <b>50</b> for 1 month
+        </p>
+        <Button color='green' onClick={this.handleClose} inverted>
+            <Icon name='checkmark' /> Choose
+          </Button>
       </Grid.Column>
-      <Grid.Column computer="6">
-       hi
+      <Grid.Column textAlign='center'>
+        
+        <p>
+          <b>250</b> for 6 month
+        </p>
+        <Button color='green' onClick={this.handleClose} inverted>
+            <Icon name='checkmark' /> Choose
+          </Button>
       </Grid.Column>
-      <Grid.Column computer="10">
-       hi
+      <Grid.Column textAlign='center'>
+       
+        <p>
+        <b>400</b> for 1 year
+        </p>
+        <Button color='green' onClick={this.handleClose} inverted>
+            <Icon name='checkmark' /> Choose
+          </Button>
+      </Grid.Column>
+      <Grid.Column textAlign='center'>
+        <Button color='red' onClick={this.handleClose} inverted>
+            <Icon name='close' />
+          </Button>
+      </Grid.Column>
+    </Grid>
+        </Modal.Content>
+        <Modal.Actions>
+        </Modal.Actions>
+      </Modal>
+ 
+
+    </div>
+      </Grid.Column>
+      <Grid.Column computer="8" style={{border:'solid .1px #00000040'}}>
+      <Step.Group>
+    <Step completed style={{padding: "17px", paddingLeft:"22px"}}>
+      <Step.Content>
+        <Step.Description>Choose your subscription</Step.Description>
+      </Step.Content>
+    </Step>
+
+    <Step completed style={{padding: "17px", paddingLeft:"22px"}}>
+      <Step.Content>
+        <Step.Description>Pay for subscription</Step.Description>
+      </Step.Content>
+    </Step>
+
+    <Step active style={{padding: "17px", paddingLeft:"22px"}}>
+      <Step.Content>
+        <Step.Description>Enjoy you subscription</Step.Description>
+      </Step.Content>
+    </Step>
+  </Step.Group>
+      </Grid.Column>
+      <Grid.Column computer="8"style={{overflow: 'scroll',maxHeight: '223px', border:'solid .1px #00000040'}}>
+      
+      <Segment.Group>
+    <Segment><h2>Albums</h2></Segment>
+    <Segment.Group>
+      <Segment>Album 1</Segment>
+      <Segment>Album 2</Segment>
+      <Segment>Album 3</Segment>
+    </Segment.Group>
+    </Segment.Group>
+    <Segment.Group>
+    <Segment><h2>Artists</h2></Segment>
+    <Segment.Group>
+      <Segment>Artist 1</Segment>
+      <Segment>Artist 2</Segment>
+      <Segment>Artist 3</Segment>
+    </Segment.Group>
+    </Segment.Group>
+
       </Grid.Column>
    
   </Grid>
-  </div>
-)
-
-
-export default Practice
+  </div>   
+    )
+  }
+}
+export default ModalExampleControlled
